e9e98397eaff9d0a42640150cbee20f1874c28a4
Boyuan Feng <boyuan.feng@outlook.com>
2017-05-29 15:48:57 -0400

    create very basic framework

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestFramework;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CanGoToHomePage()
+        {
+            Page.HomePage.Goto();
+            Assert.IsTrue(Page.HomePage.IsAt());
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            Browser.Close();
+        }
+    }
+
+}
---
+++ TestFramework/Page.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TestFramework
+{
+    public static class Page
+    {
+        public static class HomePage
+        {
+            public static string Url { get; set; }
+            private static string PageTitle = "Pluralsight";
+
+            static HomePage()
+            {
+                Url = "http://pluralsight.com";
+            }
+
+            public static void Goto()
+            {
+                Browser.Goto(Url);
+            }
+
+            public static bool IsAt()
+            {
+                return Browser.Title.Contains(PageTitle);
+            }
+        }
+    }
+
+
+}
